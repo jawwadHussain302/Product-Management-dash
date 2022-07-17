6c521b3c9fd8f1a52de8ad58b3dbe62e45349762
Jawwad Hussain <bc190406131@vu.edu.pk>
2022-07-17 15:46:36 +0500

    basic structure is ready

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ProductListComponent } from './products/product-list.component';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
